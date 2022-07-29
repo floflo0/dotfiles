50c794d426427d863633bc939520adb0072a3b50
Floris Bartra <floris.bartra@gmail.com>
2022-07-29 15:38:26 +0200

    Format options

--- nvim/.config/nvim/lua/floflo/set.lua
+++ nvim/.config/nvim/lua/floflo/set.lua
@@ -77,15 +77,16 @@ set_cursorline('WinEnter', true)
 set_cursorline('FileType', false, 'TelescopePrompt')
 
 vim.opt.formatoptions = vim.opt.formatoptions
-  - 'a' -- Auto formatting is BAD.
-  - 't' -- Don't auto format my code. I got linters for that.
-  + 'q' -- Allow formatting comments w/ gq
-  + 'n' -- Indent past the formatlistpat, not underneath it.
-  + 'j' -- Auto-remove comments if possible.
-  + 'c' -- In general, I like it when comments respect textwidth
-  - 'o' -- O and o, don't continue comments
-  + 'r' -- But do continue when pressing enter.
-  + '2'
+    - 'a' -- Auto formatting is BAD.
+    - 't' -- Don't auto format my code. I got linters for that.
+    + 'q' -- Allow formatting comments with gq
+    + 'n' -- Indent past the formatlistpat, not underneath it.
+    + 'j' -- Auto-remove comments if possible.
+    + 'c' -- In general, I like it when comments respect textwidth
+    - 'o' -- O and o, don't continue comments
+    + 'r' -- But do continue when pressing enter.
+    + '2'
+    + '/'
 
 -- transparence
 vim.opt.pumblend = 15
