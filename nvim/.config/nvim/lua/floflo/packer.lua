4231935ee3842f269abe655af33c2a8f29940da2
Floris Bartra <floris.bartra@gmail.com>
2022-08-21 11:47:33 +0200

    Move packer in neovim config

--- nvim/.config/nvim/lua/floflo/packer.lua
+++ nvim/.config/nvim/lua/floflo/packer.lua
@@ -2,7 +2,17 @@
 -- $ yay -S nvim-packer-git
 -- :PackerSync
 
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
 return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
+
     -- autocompletion
     use('neovim/nvim-lspconfig')
     use('hrsh7th/cmp-nvim-lsp')
