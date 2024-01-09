daf64962b94450b6e5212fa2777a2cdbdf0c5225
Floris Bartra <floris.bartra@gmail.com>
2024-01-09 01:07:58 +0100

    Move luansnip to his own file

---
+++ nvim/.config/nvim/lua/floflo/plugins/luasnip.lua
@@ -0,0 +1,32 @@
+return {
+    'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = 'v2.*',
+    build = 'make install_jsregexp',
+    config = function()
+        local luasnip = require('luasnip')
+        vim.keymap.set('i', '<Tab>', function()
+            if luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                return '<Tab>'
+            end
+        end, { silent = true, noremap = true, expr = true })
+        vim.keymap.set('s', '<Tab>', function()
+            luasnip.jump(1)
+        end, { silent = true, noremap = true })
+        vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+            luasnip.jump(-1)
+        end, { silent = true, noremap = true })
+
+        vim.keymap.set({ 'i', 's' }, '<C-E>', function()
+            if luasnip.choice_active() then
+                luasnip.next_choice()
+            end
+        end, { silent = true, noremap = true })
+
+        require('luasnip.loaders.from_vscode').lazy_load({
+            include = { 'html', 'markdown' }
+        })
+    end
+}
--- nvim/.config/nvim/lua/floflo/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/floflo/plugins/nvim-cmp.lua
@@ -11,38 +11,7 @@ return {
         'hrsh7th/cmp-cmdline',
         'hrsh7th/cmp-nvim-lsp-document-symbol',
         'hrsh7th/cmp-nvim-lsp-signature-help',
-        {
-            'L3MON4D3/LuaSnip',
-            dependencies = { 'rafamadriz/friendly-snippets' },
-            version = 'v2.*',
-            build = 'make install_jsregexp',
-            config = function()
-                local luasnip = require('luasnip')
-                vim.keymap.set('i', '<Tab>', function()
-                    if luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        return '<Tab>'
-                    end
-                end, { silent = true, noremap = true, expr = true })
-                vim.keymap.set('s', '<Tab>', function()
-                    luasnip.jump(1)
-                end, { silent = true, noremap = true })
-                vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
-                    luasnip.jump(-1)
-                end, { silent = true, noremap = true })
-
-                vim.keymap.set({ 'i', 's' }, '<C-E>', function()
-                    if luasnip.choice_active() then
-                        luasnip.next_choice()
-                    end
-                end, { silent = true, noremap = true })
-
-                require('luasnip.loaders.from_vscode').lazy_load({
-                    include = { 'html', 'markdown' }
-                })
-            end
-        },
+        'L3MON4D3/LuaSnip',
         'saadparwaiz1/cmp_luasnip',
         'onsails/lspkind.nvim'
     },
@@ -132,9 +101,7 @@ return {
 
         local function config(_config)
             return vim.tbl_deep_extend('force', {
-                capabilities = cmp_nvim_lsp.default_capabilities(),
-                on_attach = function()
-                end,
+                capabilities = cmp_nvim_lsp.default_capabilities()
             }, _config or {})
         end
 
