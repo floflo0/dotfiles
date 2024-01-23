d4ba82f133d9dad0310905e9ea3bf2c9806a72ac
Floris Bartra <floris.bartra@gmail.com>
2024-01-23 13:01:36 +0100

    Use vim.fs.joinpath

--- nvim/.config/nvim/cd-picker/lua/cd-picker.lua
+++ nvim/.config/nvim/cd-picker/lua/cd-picker.lua
@@ -14,10 +14,6 @@ local conf = require('telescope.config').values
 local ROOT = os.getenv('HOME')
 local PREVIEW_DEPTH = 5
 
-local function joinpath(path1, path2)
-    return vim.fn.resolve(path1 .. '/' .. path2)
-end
-
 return function()
     local previewer = previewers.new_termopen_previewer({
         get_command = function(entry)
@@ -28,7 +24,7 @@ return function()
                 '--icons',
                 '--level', PREVIEW_DEPTH,
                 '--ignore-glob=node_modules|__pycache__|vendor',
-                joinpath(ROOT, entry.value)
+                vim.fs.joinpath(ROOT, entry.value)
             }
         end,
     })
@@ -48,7 +44,7 @@ return function()
             actions.select_default:replace(function()
                 actions.close(prompt_bufnr)
                 local selection = action_state.get_selected_entry()
-                local path = joinpath(ROOT, selection[1])
+                local path = vim.fs.joinpath(ROOT, selection[1])
                 vim.cmd('cd ' .. path)
                 vim.cmd('e.')
                 vim.print(path)
