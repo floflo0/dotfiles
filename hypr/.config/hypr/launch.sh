b20e44026dee8865999ecfefba506a57827ec712
Floris Bartra <floris.bartra@gmail.com>
2025-04-02 08:19:05 +0200

    Load xressources in Hyprland

--- hypr/.config/hypr/launch.sh
+++ hypr/.config/hypr/launch.sh
@@ -2,4 +2,14 @@
 
 source "$HOME/.profile"
 
+USER_RESOURCES="${XDG_CONFIG_HOME}/xresources"
+SYS_RESOURCES="/etc/X11/xinit/.Xresources"
+
+if [ -f "$SYS_RESOURCES" ]; then
+    xrdb -merge "$SYS_RESOURCES"
+fi
+if [ -f "$USER_RESOURCES" ]; then
+    xrdb -merge "$USER_RESOURCES"
+fi
+
 exec Hyprland
